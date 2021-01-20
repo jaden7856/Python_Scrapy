6f6d8d2831ca3649af6364d8ff970b281e82ad41
Jaden7856 <jhg7856@gmail.com>
2021-01-20 11:54:06 +0900

    update mymovie

--- myscraper/myscraper/spiders/spider_bot.py
+++ myscraper/myscraper/spiders/spider_bot.py
@@ -4,7 +4,6 @@ from scrapy.http import Request
 
 
 URL = 'https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=101&sid2=258&page=%s' # news
-# URL = 'https://movie.naver.com/movie/point/af/list.nhn?&page=%s' # movie
 start_page = 1
 
 class SpiderBotSpider(scrapy.Spider):
@@ -37,21 +36,3 @@ class SpiderBotSpider(scrapy.Spider):
             items.append(item)
         
         return items
-
-
-        # movies
-        #titles = response.xpath('//*[@id="old_content"]/table/tbody/tr/td[2]/a/text()').extract()
-        #ranks = response.xpath('//*[@id="old_content"]/table/tbody/tr/td[2]/div/em/text()').extract()
-        #previews = response.xpath('//*[@id="old_content"]/table/tbody/tr/td[2]/text()').extract()
-        #writers = response.css('.author::text').extract()
-        #dates = response.xpath('//*[@id="old_content"]/table/tbody/tr/td[3]/text()').extract()
-        
-        #for idx in range(len(ranks)):
-        #    item = MyscraperItem()
-        #    item['title'] = titles[idx]
-        #    item['rank'] = ranks[idx]
-        #    item['preview'] = previews[idx]
-        #    item['writer'] = writers[idx]
-        #    item['date'] = dates[idx]
-
-        #    items.append(item)
